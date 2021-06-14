ed1b6ecd93977a3e33c224476e5c0b21b6c0d2b3
anthony abrignani <anthony abrignani>
2021-06-14 13:13:40 -0400

    google login

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,21 @@
+import { AuthContext } from "../context/auth-context";
+import { useContext } from "react";
+import { signInWithGoogle, signOut } from "../firebaseConfig";
+
 
 function Header(){
+    const { user } = useContext(AuthContext)
+    
     return(
-        <div className="Header">
-            <h1>Read it and Weep</h1>
-        </div>
-
+        <header className="Header">
+            <h1>Read It and Weep</h1>
+            { !user ? <button onClick={signInWithGoogle}>Sign In With Google</button> :
+            <button onClick={signOut}>Sign Out</button>}
+            { user && <div>
+                <h3>Welcome {user.displayName}!</h3>
+                { !!user.photoURL && <img src={user.photoURL} alt="" />}
+            </div> }
+        </header>
     )
 }
 
