2fd101908f99f4edc0a9aff2b9d34649e3e821fc
katie <kholland210@gmail.com>
2021-06-14 14:16:24 -0400

    test

--- frontend/src/components/CompetitionDisplay.tsx
+++ frontend/src/components/CompetitionDisplay.tsx
@@ -2,6 +2,7 @@
 function CompetitionDisplay(){
     return(
         <div className="CompetitionDisplay">
+            <h1> HI </h1>
            <h3>Competition Name</h3>
            <h3>Competition End Date</h3>
            <h4>Pages</h4>
